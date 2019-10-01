5d73bfa2068dba7287788f61690efff9f82c9655
lpweston <l.weston@outlook.com>
2019-10-01 17:05:03 +0100

    set up image db

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,8 +1,17 @@
 from app import db
 
 class Image(db.Model):
-    image_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     image_url = db.Column(db.String(64))
+    pieces = db.relationship('Piece', backref='ref_image', lazy='dynamic')
 
-    def __repr__(img):
-        return '<Img {}>'.format(img.image_url)    
+    def __repr__(self):
+        return '<Img {}>'.format(self.image_url)    
+
+class Piece(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    piece_url = db.Column(db.String(64))
+    image_id = db.Column(db.Integer, db.ForeignKey('image.id'))
+
+    def __repr__(self):
+        return '<Piece {}>'.format(self.piece_url) 
